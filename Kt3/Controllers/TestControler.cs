57d5aa7d971e1741fb4e95f18230b3535f94418b
Sergola05 <poryadin.serg@yandex.ru>
2025-02-03 12:25:16 +0300

    Action Text

---
+++ Kt3/Controllers/TestControler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kt3.Controllers
+{
+    public class TestControler : Controller
+    {
+        public IActionResult Text()
+        {
+            HttpContext.Response.ContentType = "text/plain";
+            return new ContentResult { Content = "Hello, world!" };
+        }
+    }
+}
